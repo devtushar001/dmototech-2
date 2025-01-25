5409d25b1ab572fbd48d29afc12a8a2a1ec3aa15
devtushar001 <tc341269@gmail.com>
2025-01-25 02:54:05 -0500

    New design of website

--- client/src/components/StarsIcon/StarIcon.jsx
+++ client/src/components/StarsIcon/StarIcon.jsx
@@ -3,13 +3,57 @@ import './StarIcon.css'
 
 const StarIcon = () => {
     const [ratings, setRatings] = useState(0);
+    const [reviews, setReviews] = useState(false)
     useEffect(() => {
     }, [ratings])
     return (
         <>
             <div className="star-icon">
+                <div className="our-previos-review">
+                    <h2>Top reviews.</h2>
+                    <br />
+                    <div className="reviews-container">
+                        <div className="review-1">
+                            <p> elit. um id, Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ratione cupiditate? Debitis! velit veniam quod illum dicta tempora harum vero?</p>
+                            <div className="user-name">
+                                <p>Mr. Tusahr</p>
+                                <div className="user-ratings">
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="review-1">
+                            <p> elit. um id, Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ratione cupiditate? Debitis! velit veniam quod illum dicta tempora harum vero?</p>
+                            <div className="user-name">
+                                <p>Mr. Tusahr</p>
+                                <div className="user-ratings">
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="review-1">
+                            <p> elit. um id, Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ratione cupiditate? Debitis! velit veniam quod illum dicta tempora harum vero?</p>
+                            <div className="user-name">
+                                <p>Mr. Tusahr</p>
+                                <div className="user-ratings">
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                    <span style={{ color: "goldenrod", fontSize: "170%" }}>★</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br />
                 <div className="rate-us">
-                    <h2>Rate US</h2>
+                    <h2>Rate DMOTOTECH</h2>
                     <div className="rated-stars">
                         <span
                             onClick={() => setRatings(1)}
@@ -41,8 +85,8 @@ const StarIcon = () => {
                 {ratings > 0 ? <div className="rating-message">
                     <textarea name="message" id="message"></textarea>
                     <div className="buttons">
-                        <button>Submit</button>
-                        <button>Cancel</button>
+                        <button className="cancel" onClick={() => setRatings(0)}>Cancel</button>
+                        <button className="submit">Submit</button>
                     </div>
                 </div> : ""}
             </div>
