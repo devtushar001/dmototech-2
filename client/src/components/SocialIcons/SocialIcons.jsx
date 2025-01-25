7815eb568574b635b36bf13ed7acc1beb30f032d
devtushar001 <tc341269@gmail.com>
2025-01-25 08:17:17 -0500

    dist changes.

--- client/src/components/SocialIcons/SocialIcons.jsx
+++ client/src/components/SocialIcons/SocialIcons.jsx
@@ -6,9 +6,9 @@ const SocialIcons = () => {
    return (
       <>
          <div className="social-icons">
-            <img src={assets.whatsapp} alt="" />
-            <img src={assets.facebook_icon} alt="" />
-            <img src={assets.instagram_icon} alt="" />
+            <a href="https://wa.me/918806795165" target="_blank"><img src={assets.whatsapp} alt="" /> </a>
+            <a href="https://www.facebook.com/dochaki/" target="_blank"><img src={assets.facebook_icon} alt="" /></a>
+            <a href="https://www.instagram.com/dmototech/" target="_blank"><img src={assets.instagram_icon} alt="" /></a>
          </div>
       </>
    )
